3a6e2f5ea92a011916f855165191c28839cfd5b8
Kevin Villegas <kevin.villegas94@gmail.com>
2022-06-07 14:56:00 -0700

    added apiurl file. Added get by id route and request in single item component. Fixed bugs in app component.

---
+++ src/apiUrl.js
@@ -0,0 +1,13 @@
+let apiUrl;
+const apiUrls = {
+    production: "https://anime-mern-backend.herokuapp.com",
+    development: "http://localhost:4000",
+};
+
+if (window.location.hostname === "localhost") {
+    apiUrl = apiUrls.development;
+} else {
+    apiUrl = apiUrls.production;
+}
+
+export default apiUrl;
--- src/components/AnimeItem.js
+++ src/components/AnimeItem.js
@@ -1,5 +1,52 @@
-const AnimeItem = () => {
-    return <div>single item</div>;
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import apiUrl from "../apiUrl";
+
+const AnimeItem = (props) => {
+    const params = useParams();
+    let navigate = useNavigate();
+    const [id, setId] = useState([]);
+    const [name, setName] = useState("");
+    const [year, setYear] = useState("");
+    const [genre, setGenre] = useState("");
+
+    useEffect(() => {
+        getReq();
+    }, []);
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+    const handleYearChange = (event) => {
+        setYear(event.target.value);
+    };
+    const handleGenreChange = (event) => {
+        setGenre(event.target.value);
+    };
+
+    const getReq = async () => {
+        try {
+            let response = await axios.get(`${apiUrl}/animes/${params._id}`);
+            console.log(response.data);
+            setId(response.data.animes);
+            console.log(id);
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
+
+    return (
+        <div>
+            <ul>
+                <li>
+                    <p>Name: {id.name}</p>
+                    <p>Year Released: {id.yearReleased}</p>
+                    <p>Genre: {id.genre}</p>
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default AnimeItem;
