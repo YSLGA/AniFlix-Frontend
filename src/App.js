0b6515313f5b7199fee156f84963b0fc94455b3d
Kevin Villegas <kevin.villegas94@gmail.com>
2022-06-08 14:40:06 -0700

    added anime movies and added forms, input, routes, links and all necessary functions and created new home component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import axios from "axios";
-import Home from "./components/Home";
-import AnimeItem from "./components/AnimeItem";
+import AnimeShows from "./components/AnimeShows";
+import AnimeShowItem from "./components/AnimeShowItem";
 import apiUrl from "./apiUrl";
 import Team from "./components/Team";
+import Home from "./Home";
+import AnimeMovies from "./components/AnimeMovies";
+import AnimeMovieItem from "./components/AnimeMovieItem";
 
 const App = () => {
     const [animes, setAnimes] = useState([]);
+    const [movies, setMovies] = useState([]);
 
     useEffect(() => {
         getAnimes();
+        getMovies();
     }, []);
 
     const getAnimes = async () => {
@@ -23,14 +28,37 @@ const App = () => {
         }
     };
 
+    const getMovies = async () => {
+        try {
+            let response = await axios.get(`${apiUrl}/animeMovies`);
+            console.log(response.data);
+            setMovies(response.data.movies);
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
+
     const displayAnime = animes.map((anime) => {
         return (
-            <Link to={"/" + anime._id}>
+            <Link to={"/animes/" + anime._id}>
                 <li key={anime._id}>
                     <p>Name: {anime.name}</p>
                     <p>Year Released: {anime.yearReleased}</p>
                     <p>Genre: {anime.genre}</p>
-                    <img src={anime.image} />
+                    <img src={anime.image} alt="anime cover" />
+                </li>
+            </Link>
+        );
+    });
+
+    const displayMovies = movies.map((movie, key) => {
+        return (
+            <Link to={"/movies/" + movie._id}>
+                <li key={key}>
+                    <p>Title: {movie.title}</p>
+                    <p>Year Released: {movie.yearReleased}</p>
+                    <p>Genre: {movie.genre}</p>
+                    <img src={movie.image} alt="anime movie cover" />
                 </li>
             </Link>
         );
@@ -40,25 +68,40 @@ const App = () => {
         <div>
             <nav>
                 <ul>
-                    <Link to="/">
-                        <li>Home</li>
-                    </Link>
-                    <Link to="/Team">
-                        <li>Team</li>
-                    </Link>
+                    <Link to="/">Home</Link>
+                    <Link to="/animes">Anime Shows</Link>
+                    <Link to="/movies">Anime Movies</Link>
+                    <Link to="/Team">Team</Link>
                 </ul>
             </nav>
 
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route
-                    path="/"
+                    path="/animes/"
                     element={
-                        <Home animes={displayAnime} setAnime={setAnimes} />
+                        <AnimeShows
+                            animes={displayAnime}
+                            setAnime={setAnimes}
+                        />
                     }
                 />
                 <Route
-                    path="/:_id"
-                    element={<AnimeItem setAnime={setAnimes} />}
+                    path="/animes/:_id"
+                    element={<AnimeShowItem setAnime={setAnimes} />}
+                />
+                <Route
+                    path="/movies/"
+                    element={
+                        <AnimeMovies
+                            movies={displayMovies}
+                            setMovies={setMovies}
+                        />
+                    }
+                />
+                <Route
+                    path="/movies/:_id"
+                    element={<AnimeMovieItem setMovies={setMovies} />}
                 />
                 <Route path="/Team" element={<Team />} />
             </Routes>
---
+++ src/components/AnimeMovies.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import { useState } from "react";
+import apiUrl from "../apiUrl";
+
+const AnimeMovies = (props) => {
+    const [name, setName] = useState("");
+    const [year, setYear] = useState("");
+    const [genre, setGenre] = useState("");
+    const [image, setImage] = useState("");
+
+    const postAnimeMovie = async () => {
+        let payload = {
+            name: name,
+            yearReleased: year,
+            genre: genre,
+            image: image,
+        };
+        try {
+            let response = await axios.post(`${apiUrl}/animeMovies/`, payload);
+            console.log(response.data);
+        } catch (ex) {
+            console.log(ex);
+        }
+        try {
+            let response = await axios.get(`${apiUrl}/animeMovies/`);
+            console.log(response.data);
+            props.setAnime(response.data.movies);
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        postAnimeMovie();
+        setName("");
+        setYear("");
+        setGenre("");
+        setImage("");
+    };
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+    const handleYearChange = (event) => {
+        setYear(event.target.value);
+    };
+    const handleGenreChange = (event) => {
+        setGenre(event.target.value);
+    };
+    const handleImageChange = (event) => {
+        setImage(event.target.value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={handleNameChange}
+                    value={name}
+                    type="text"
+                    placeholder="name"
+                />
+                <input
+                    onChange={handleYearChange}
+                    value={year}
+                    type="text"
+                    placeholder="year released"
+                />
+                <input
+                    onChange={handleGenreChange}
+                    value={genre}
+                    type="text"
+                    placeholder="genre"
+                />
+                <input
+                    type="text"
+                    value={image}
+                    onChange={handleImageChange}
+                    placeholder="image"
+                />
+                <input type="submit" value="add to list" />
+            </form>
+            <ul>{props.movies}</ul>
+        </div>
+    );
+};
+
+export default AnimeMovies;
