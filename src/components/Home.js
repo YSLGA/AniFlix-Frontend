080799406f77f8bf7929f8ffedc037c56cfa7090
zeke <ezekielpaints@gmail.com>
2022-06-08 13:00:31 -0400

    added user input field

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,71 +3,86 @@ import { useState } from "react";
 import apiUrl from "../apiUrl";
 
 const Home = (props) => {
-    const [name, setName] = useState("");
-    const [year, setYear] = useState("");
-    const [genre, setGenre] = useState("");
+  const [name, setName] = useState("");
+  const [year, setYear] = useState("");
+  const [genre, setGenre] = useState("");
+  const [user, setUser] = useState("");
 
-    const postAnime = async () => {
-        let payload = { name: name, yearReleased: year, genre: genre };
-        try {
-            let response = await axios.post(`${apiUrl}/animes/`, payload);
-            console.log(response.data);
-        } catch (ex) {
-            console.log(ex);
-        }
-        try {
-            let response = await axios.get(`${apiUrl}/animes/`);
-            console.log(response.data);
-            props.setAnime(response.data.animes);
-        } catch (ex) {
-            console.log(ex);
-        }
-    };
+  const postAnime = async () => {
+    let payload = { name: name, yearReleased: year, genre: genre };
+    try {
+      let response = await axios.post(`${apiUrl}/animes/`, payload);
+      console.log(response.data);
+    } catch (ex) {
+      console.log(ex);
+    }
+    try {
+      let response = await axios.get(`${apiUrl}/animes/`);
+      console.log(response.data);
+      props.setAnime(response.data.animes);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        postAnime();
-        setName("");
-        setYear("");
-        setGenre("");
-    };
+  ////////////////////////////////
 
-    const handleNameChange = (event) => {
-        setName(event.target.value);
-    };
-    const handleYearChange = (event) => {
-        setYear(event.target.value);
-    };
-    const handleGenreChange = (event) => {
-        setGenre(event.target.value);
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    postAnime();
+    setName("");
+    setYear("");
+    setGenre("");
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    onChange={handleNameChange}
-                    value={name}
-                    type="text"
-                    placeholder="name"
-                />
-                <input
-                    onChange={handleYearChange}
-                    value={year}
-                    type="text"
-                    placeholder="year released"
-                />
-                <input
-                    onChange={handleGenreChange}
-                    value={genre}
-                    type="text"
-                    placeholder="genre"
-                />
-                <input type="submit" value="add to list" />
-            </form>
-            <ul>{props.animes}</ul>
-        </div>
-    );
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleYearChange = (event) => {
+    setYear(event.target.value);
+  };
+  const handleGenreChange = (event) => {
+    setGenre(event.target.value);
+  };
+
+  const handleUserChange = (event) => {
+    setUser(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleNameChange}
+          value={name}
+          type="text"
+          placeholder="name"
+        />
+        <input
+          onChange={handleYearChange}
+          value={year}
+          type="text"
+          placeholder="year released"
+        />
+        <input
+          onChange={handleGenreChange}
+          value={genre}
+          type="text"
+          placeholder="genre"
+        />
+
+        <input
+          onChange={handleUserChange}
+          value={user}
+          type="text"
+          placeholder="username"
+        />
+
+        <input type="submit" value="add to list" />
+      </form>
+      <ul>{props.animes}</ul>
+    </div>
+  );
 };
 
 export default Home;
