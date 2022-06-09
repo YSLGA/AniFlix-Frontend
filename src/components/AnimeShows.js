090011d15a8dae757ea8adeed108965a0641b9a0
zeke <ezekielpaints@gmail.com>
2022-06-09 14:05:29 -0400

    team page

--- src/components/AnimeShows.js
+++ src/components/AnimeShows.js
@@ -3,87 +3,87 @@ import { useState } from "react";
 import apiUrl from "../apiUrl";
 
 const AnimeShows = (props) => {
-    const [name, setName] = useState("");
-    const [year, setYear] = useState("");
-    const [genre, setGenre] = useState("");
-    const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [year, setYear] = useState("");
+  const [genre, setGenre] = useState("");
+  const [image, setImage] = useState("");
 
-    const postAnime = async () => {
-        let payload = {
-            name: name,
-            yearReleased: year,
-            genre: genre,
-            image: image,
-        };
-        try {
-            let response = await axios.post(`${apiUrl}/animes/`, payload);
-            console.log(response.data);
-        } catch (ex) {
-            console.log(ex);
-        }
-        try {
-            let response = await axios.get(`${apiUrl}/animes/`);
-            console.log(response.data);
-            props.setAnime(response.data.animes);
-        } catch (ex) {
-            console.log(ex);
-        }
+  const postAnime = async () => {
+    let payload = {
+      name: name,
+      yearReleased: year,
+      genre: genre,
+      image: image,
     };
+    try {
+      let response = await axios.post(`${apiUrl}/animes/`, payload);
+      console.log(response.data);
+    } catch (ex) {
+      console.log(ex);
+    }
+    try {
+      let response = await axios.get(`${apiUrl}/animes/`);
+      console.log(response.data);
+      props.setAnime(response.data.animes);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        postAnime();
-        setName("");
-        setYear("");
-        setGenre("");
-        setImage("");
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    postAnime();
+    setName("");
+    setYear("");
+    setGenre("");
+    setImage("");
+  };
 
-    const handleNameChange = (event) => {
-        setName(event.target.value);
-    };
-    const handleYearChange = (event) => {
-        setYear(event.target.value);
-    };
-    const handleGenreChange = (event) => {
-        setGenre(event.target.value);
-    };
-    const handleImageChange = (event) => {
-        setImage(event.target.value);
-    };
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleYearChange = (event) => {
+    setYear(event.target.value);
+  };
+  const handleGenreChange = (event) => {
+    setGenre(event.target.value);
+  };
+  const handleImageChange = (event) => {
+    setImage(event.target.value);
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    onChange={handleNameChange}
-                    value={name}
-                    type="text"
-                    placeholder="name"
-                />
-                <input
-                    onChange={handleYearChange}
-                    value={year}
-                    type="text"
-                    placeholder="year released"
-                />
-                <input
-                    onChange={handleGenreChange}
-                    value={genre}
-                    type="text"
-                    placeholder="genre"
-                />
-                <input
-                    type="text"
-                    value={image}
-                    onChange={handleImageChange}
-                    placeholder="image"
-                />
-                <input type="submit" value="add to list" />
-            </form>
-            <ul>{props.animes}</ul>
-        </div>
-    );
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleNameChange}
+          value={name}
+          type="text"
+          placeholder="name"
+        />
+        <input
+          onChange={handleYearChange}
+          value={year}
+          type="text"
+          placeholder="year released"
+        />
+        <input
+          onChange={handleGenreChange}
+          value={genre}
+          type="text"
+          placeholder="genre"
+        />
+        <input
+          type="text"
+          value={image}
+          onChange={handleImageChange}
+          placeholder="image"
+        />
+        <input type="submit" value="add to list" />
+      </form>
+      <ul>{props.animes}</ul>
+    </div>
+  );
 };
 
 export default AnimeShows;
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,5 +1,25 @@
 const Team = () => {
-  return <div>Imran Zeke Kevin</div>;
+  return (
+    <div>
+      <h1> Meet The Devs </h1>
+
+      <img src="imran.png" alt="Imran Syed"></img>
+      <p>Planned, designed, and deployed database and website</p>
+
+      <img src="kevin.png" alt="Kevin Villegas"></img>
+      <p>Planned, designed, and lead database and website construction</p>
+
+      <img src="zeke.jpg" alt="Ezekiel Robinson"></img>
+      <p>Planned, designed, and contributed to database and website creation</p>
+
+      <p>
+        {" "}
+        Three fellas with a thorough appreciation for anime and discovery! We
+        hope you enjoy our site. Keep posting anime movies and TV shows, to help
+        us grow our library!{" "}
+      </p>
+    </div>
+  );
 };
 
 export default Team;
