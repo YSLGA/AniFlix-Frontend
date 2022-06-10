4f9573d3a6a0eabcc323ccfa65c3c4a2037e0519
Kevin Villegas <kevin.villegas94@gmail.com>
2022-06-10 08:19:37 -0700

    added responsive design

--- src/components/AnimeMovieItem.js
+++ src/components/AnimeMovieItem.js
@@ -4,130 +4,143 @@ import axios from "axios";
 import apiUrl from "../apiUrl";
 
 const AnimeMovieItem = (props) => {
-  const params = useParams();
-  let navigate = useNavigate();
-  const [id, setId] = useState([]);
-  const [title, setTitle] = useState("");
-  const [year, setYear] = useState("");
-  const [genre, setGenre] = useState("");
-  const [image, setImage] = useState("");
+    const params = useParams();
+    let navigate = useNavigate();
+    const [id, setId] = useState([]);
+    const [title, setTitle] = useState("");
+    const [year, setYear] = useState("");
+    const [genre, setGenre] = useState("");
+    const [image, setImage] = useState("");
 
-  useEffect(() => {
-    getReq();
-  }, []);
+    useEffect(() => {
+        getReq();
+    }, []);
 
-  const handleNameChange = (event) => {
-    setTitle(event.target.value);
-  };
-  const handleYearChange = (event) => {
-    setYear(event.target.value);
-  };
-  const handleGenreChange = (event) => {
-    setGenre(event.target.value);
-  };
-  const handleImageChange = (event) => {
-    setImage(event.target.value);
-  };
-
-  const patchReq = async () => {
-    let payload = {
-      title: title,
-      yearReleased: year,
-      genre: genre,
-      image: image,
+    const handleNameChange = (event) => {
+        setTitle(event.target.value);
     };
-    try {
-      let response = await axios.patch(
-        `${apiUrl}/animeMovies/${params._id}`,
-        payload
-      );
-      console.log(response.data.movies);
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
+    const handleYearChange = (event) => {
+        setYear(event.target.value);
+    };
+    const handleGenreChange = (event) => {
+        setGenre(event.target.value);
+    };
+    const handleImageChange = (event) => {
+        setImage(event.target.value);
+    };
 
-  const handleSubmit = () => {
-    patchReq();
-    setTitle("");
-    setYear("");
-    setGenre("");
-    setImage("");
-  };
+    const patchReq = async () => {
+        let payload = {
+            title: title,
+            yearReleased: year,
+            genre: genre,
+            image: image,
+        };
+        try {
+            let response = await axios.patch(
+                `${apiUrl}/animeMovies/${params._id}`,
+                payload
+            );
+            console.log(response.data.movies);
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
 
-  const deleteReq = async () => {
-    try {
-      let response = await axios.delete(`${apiUrl}/animeMovies/${params._id}`);
-      console.log(response.data);
-    } catch (ex) {
-      console.log(ex);
-    }
-    try {
-      let response = await axios.get(`${apiUrl}/animeMovies/`);
-      console.log(response.data);
-      props.setMovies(response.data.movies);
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
+    const handleSubmit = () => {
+        patchReq();
+        setTitle("");
+        setYear("");
+        setGenre("");
+        setImage("");
+    };
 
-  const handleClick = () => {
-    deleteReq();
-    navigate("/");
-  };
+    const deleteReq = async () => {
+        try {
+            let response = await axios.delete(
+                `${apiUrl}/animeMovies/${params._id}`
+            );
+            console.log(response.data);
+        } catch (ex) {
+            console.log(ex);
+        }
+        try {
+            let response = await axios.get(`${apiUrl}/animeMovies/`);
+            console.log(response.data);
+            props.setMovies(response.data.movies);
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
 
-  const getReq = async () => {
-    try {
-      let response = await axios.get(`${apiUrl}/animeMovies/${params._id}`);
-      console.log(response.data);
-      setId(response.data.movies);
-      console.log(id);
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
+    const handleClick = () => {
+        deleteReq();
+        navigate("/movies");
+    };
 
-  return (
-    <div className="animeMovieItem">
-      <form className="animeMovieItem-text-fields" onSubmit={handleSubmit}>
-        <input
-          onChange={handleNameChange}
-          value={title}
-          type="text"
-          placeholder="name"
-        />
-        <input
-          onChange={handleYearChange}
-          value={year}
-          type="text"
-          placeholder="year released"
-        />
-        <input
-          onChange={handleGenreChange}
-          value={genre}
-          type="text"
-          placeholder="genre"
-        />
-        <input
-          type="text"
-          value={image}
-          onChange={handleImageChange}
-          placeholder="image"
-        />
-        <input type="submit" value="edit" />
-        <button className="delete-btn" onClick={handleClick}>delete</button>
-      </form>
+    const getReq = async () => {
+        try {
+            let response = await axios.get(
+                `${apiUrl}/animeMovies/${params._id}`
+            );
+            console.log(response.data);
+            setId(response.data.movies);
+            console.log(id);
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
 
-      <ul>
-        <li>
-          <p>Title: {id.title}</p>
-          <p>Year Released: {id.yearReleased}</p>
-          <p>Genre: {id.genre}</p>
-          <img className="anime-item" src={id.image} alt="single anime movie cover" />
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className="animeMovieItem">
+            <form
+                className="animeMovieItem-text-fields"
+                onSubmit={handleSubmit}
+            >
+                <input
+                    onChange={handleNameChange}
+                    value={title}
+                    type="text"
+                    placeholder="name"
+                />
+                <input
+                    onChange={handleYearChange}
+                    value={year}
+                    type="text"
+                    placeholder="year released"
+                />
+                <input
+                    onChange={handleGenreChange}
+                    value={genre}
+                    type="text"
+                    placeholder="genre"
+                />
+                <input
+                    type="text"
+                    value={image}
+                    onChange={handleImageChange}
+                    placeholder="image"
+                />
+                <input type="submit" value="edit" />
+                <button className="delete-btn" onClick={handleClick}>
+                    delete
+                </button>
+            </form>
+
+            <ul>
+                <li>
+                    <p>Title: {id.title}</p>
+                    <p>Year Released: {id.yearReleased}</p>
+                    <p>Genre: {id.genre}</p>
+                    <img
+                        className="anime-item"
+                        src={id.image}
+                        alt="single anime movie cover"
+                    />
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default AnimeMovieItem;
--- src/components/AnimeShowItem.js
+++ src/components/AnimeShowItem.js
@@ -4,130 +4,136 @@ import axios from "axios";
 import apiUrl from "../apiUrl";
 
 const AnimeShowItem = (props) => {
-  const params = useParams();
-  let navigate = useNavigate();
-  const [id, setId] = useState([]);
-  const [name, setName] = useState("");
-  const [year, setYear] = useState("");
-  const [genre, setGenre] = useState("");
-  const [image, setImage] = useState("");
+    const params = useParams();
+    let navigate = useNavigate();
+    const [id, setId] = useState([]);
+    const [name, setName] = useState("");
+    const [year, setYear] = useState("");
+    const [genre, setGenre] = useState("");
+    const [image, setImage] = useState("");
 
-  useEffect(() => {
-    getReq();
-  }, []);
+    useEffect(() => {
+        getReq();
+    }, []);
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-  const handleYearChange = (event) => {
-    setYear(event.target.value);
-  };
-  const handleGenreChange = (event) => {
-    setGenre(event.target.value);
-  };
-  const handleImageChange = (event) => {
-    setImage(event.target.value);
-  };
-
-  const patchReq = async () => {
-    let payload = {
-      name: name,
-      yearReleased: year,
-      genre: genre,
-      image: image,
+    const handleNameChange = (event) => {
+        setName(event.target.value);
     };
-    try {
-      let response = await axios.patch(
-        `${apiUrl}/animes/${params._id}`,
-        payload
-      );
-      console.log(response.data.animes);
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
+    const handleYearChange = (event) => {
+        setYear(event.target.value);
+    };
+    const handleGenreChange = (event) => {
+        setGenre(event.target.value);
+    };
+    const handleImageChange = (event) => {
+        setImage(event.target.value);
+    };
 
-  const handleSubmit = () => {
-    patchReq();
-    setName("");
-    setYear("");
-    setGenre("");
-    setImage("");
-  };
+    const patchReq = async () => {
+        let payload = {
+            name: name,
+            yearReleased: year,
+            genre: genre,
+            image: image,
+        };
+        try {
+            let response = await axios.patch(
+                `${apiUrl}/animes/${params._id}`,
+                payload
+            );
+            console.log(response.data.animes);
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
 
-  const deleteReq = async () => {
-    try {
-      let response = await axios.delete(`${apiUrl}/animes/${params._id}`);
-      console.log(response.data);
-    } catch (ex) {
-      console.log(ex);
-    }
-    try {
-      let response = await axios.get(`${apiUrl}/animes/`);
-      console.log(response.data);
-      props.setAnime(response.data.animes);
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
+    const handleSubmit = () => {
+        patchReq();
+        setName("");
+        setYear("");
+        setGenre("");
+        setImage("");
+    };
 
-  const handleClick = () => {
-    deleteReq();
-    navigate("/");
-  };
+    const deleteReq = async () => {
+        try {
+            let response = await axios.delete(`${apiUrl}/animes/${params._id}`);
+            console.log(response.data);
+        } catch (ex) {
+            console.log(ex);
+        }
+        try {
+            let response = await axios.get(`${apiUrl}/animes/`);
+            console.log(response.data);
+            props.setAnime(response.data.animes);
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
 
-  const getReq = async () => {
-    try {
-      let response = await axios.get(`${apiUrl}/animes/${params._id}`);
-      console.log(response.data);
-      setId(response.data.animes);
-      console.log(id);
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
+    const handleClick = () => {
+        deleteReq();
+        navigate("/animes");
+    };
 
-  return (
-    <div className="animeShowItem">
-      <form className="animeShowItem-text-fields" onSubmit={handleSubmit}>
-        <input
-          onChange={handleNameChange}
-          value={name}
-          type="text"
-          placeholder="name"
-        />
-        <input
-          onChange={handleYearChange}
-          value={year}
-          type="text"
-          placeholder="year released"
-        />
-        <input
-          onChange={handleGenreChange}
-          value={genre}
-          type="text"
-          placeholder="genre"
-        />
-        <input
-          type="text"
-          value={image}
-          onChange={handleImageChange}
-          placeholder="image"
-        />
-        <input type="submit" value="edit" />
-        <button className="delete-btn" onClick={handleClick}>delete</button>
-      </form>
+    const getReq = async () => {
+        try {
+            let response = await axios.get(`${apiUrl}/animes/${params._id}`);
+            console.log(response.data);
+            setId(response.data.animes);
+            console.log(id);
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
 
-      <ul>
-        <li>
-          <p>Title: {id.name}</p>
-          <p>Year Released: {id.yearReleased}</p>
-          <p>Genre: {id.genre}</p>
-          <img className="anime-item"src={id.image} alt="single anime cover" />
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className="animeShowItem">
+            <form className="animeShowItem-text-fields" onSubmit={handleSubmit}>
+                <input
+                    onChange={handleNameChange}
+                    value={name}
+                    type="text"
+                    placeholder="name"
+                />
+                <input
+                    onChange={handleYearChange}
+                    value={year}
+                    type="text"
+                    placeholder="year released"
+                />
+                <input
+                    onChange={handleGenreChange}
+                    value={genre}
+                    type="text"
+                    placeholder="genre"
+                />
+                <input
+                    type="text"
+                    value={image}
+                    onChange={handleImageChange}
+                    placeholder="image"
+                />
+                <input type="submit" value="edit" />
+                <button className="delete-btn" onClick={handleClick}>
+                    delete
+                </button>
+            </form>
+
+            <ul>
+                <li>
+                    <p>Title: {id.name}</p>
+                    <p>Year Released: {id.yearReleased}</p>
+                    <p>Genre: {id.genre}</p>
+                    <img
+                        className="anime-item"
+                        src={id.image}
+                        alt="single anime cover"
+                    />
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default AnimeShowItem;
